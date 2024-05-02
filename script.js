ce14adb16ee071ec0de722a00cfc81e1b328122f
kjack82 <kprothero1702@gmail.com>
2024-05-02 13:13:09 +0100

    completed basic tasks

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// Write your code here
+const lightbulb1 = document.querySelector("#lightbulb1");
+const lightbulb2 = document.querySelector("#lightbulb2");
+const lightbulb3 = document.querySelector("#lightbulb3");
+
+const subTitle = document.querySelector(".subtitle");
+
+let count = 0;
+
+function addOne(event){
+    console.log(event.target.classList);
+    count++;
+    subTitle.innerHTML = "You've clicked the lights " + count + " times";
+    event.classList.toggle("active");
+}
+
+lightbulb1.addEventListener("click", addOne);
+lightbulb2.addEventListener("click", addOne);
+lightbulb3.addEventListener("click", addOne);
